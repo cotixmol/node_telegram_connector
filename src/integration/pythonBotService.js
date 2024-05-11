ee91591f9ba122676071eac061021ecd852a0e05
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-05-11 09:36:25 -0300

    formatted message to send to Python Bot

--- src/integration/pythonBotService.js
+++ src/integration/pythonBotService.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 const sendMessageToPythonService = async (message) => {
-  const response = await axios.post(process.env.PYTHON_BOT_SERVICE_URL, message);
-  return response.data;
+
+  const formattedMessage = {
+    user_id: message.chat.id,
+    text: message.text,
+    first_name: message.chat.first_name,
+    last_name: message.chat.last_name
+  };
+
+  // await axios.post(process.env.PYTHON_BOT_SERVICE_URL, formattedMessage);
+  return {"message":formattedMessage};
 };
 
 export default { sendMessageToPythonService };
