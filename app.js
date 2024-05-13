4073bfe861fecd16f4c0aae4f44165f78ad12bb9
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-05-13 00:19:25 -0300

    WIP: Uploading to AWS EB

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.post('/webhook/telegram', telegramMessageController.handleIncomingTelegramMessage);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 export default app;
